1375ae9c9029c5f81e0e71d8ff4deee18988ab8f
Luis Novo <luis.novo@gmail.com>
2020-04-02 00:18:48 +0200

    Refactor for single box project

--- template.js
+++ template.js
@@ -1,6 +1,6 @@
 /*
  * grunt-init-ansible-template
- * 
+ *
  * Copyright (c) 2020 Luis Novo
  * Licensed under the MIT license
  */
@@ -8,14 +8,18 @@
 'use strict';
 
  // Basic template description.
-exports.description = 'Create an ansible playbook including all directories and a Vagrantfile for multibox projects';
+exports.description = 'Create an ansible playbook including all directories and a Vagrantfile for single box project';
 exports.warnOn = '*';
 // The actual init template.
 exports.template = function(grunt, init, done) {
   init.process({type: 'jquery'}, [
     //Prompt for these values
     init.prompt('name'),
-    init.prompt('description', 'Best ansible playbook')
+    init.prompt('description', 'ansible playbook'),
+    init.prompt('box', 'Vagrant box id. Type one of centos/7, ubuntu/disco64'),
+    init.prompt('hostname', 'host name: same as project name recommended'),
+    init.prompt('domain', 'golili.net'),
+    init.prompt('vbname', 'Virtual box name: same as project name recommended')
   ], function(err,props) {
     props.keywords = [];
 
